17d2ac962c3cf70ac279aa0b6a6ed92ad766ee81
Anna-Chuhlanceva <an.chuh89@gmail.com>
2024-12-04 10:39:57 +0500

    цикл фор- выводим простые числа

--- cikl FOR.py
+++ cikl FOR.py
@@ -1,20 +1,16 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20]
 primes = []
 not_primes = []
 for i in numbers:
+    if i == 1:
+        continue
     for j in range(2, i):
-        #flag = True
-        #type(j) == int
         if i % j == 0:
             not_primes.append(i)
-        else:
-            flag = False
-        break
-    #i >= j
-    #j = numbers [i]
-    #if  i % j == 0:
-     #primes [i]
-     #print (primes[i])
-    #else:
-     # primes[i]
-print (not_primes)
+            break
+    if not i in not_primes:
+        primes.append(i)
+           # break
+print (" составные числа: ", not_primes)
+print (" простые числа: ", primes)
+

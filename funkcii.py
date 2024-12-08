7b73b51f702423eb991fe2c9ce61f75bb533a123
Anna-Chuhlanceva <an.chuh89@gmail.com>
2024-12-08 21:30:28 +0500

    создаем матрицу

--- funkcii.py
+++ funkcii.py
@@ -11,7 +11,6 @@ result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
 print(result1 , result2, result3, sep="\n")
-
 #print(result2)
 #print(result3))
 #print(result3)
